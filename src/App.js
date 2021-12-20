685b44f59ad63b4d019081bcdde34d353f0e6063
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2021-12-20 11:41:53 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,83 @@
-import React , {useState} from 'react';
-import './Styles/App.scss';
-import Songs from './Components/Songs';
-import Player from './Components/Player';
-import Data from './Components/data.json'
-import Library from './Components/Library';
+import React, { useState, useRef } from "react";
+import "./styles/app.scss";
+//Import Components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+//Import data
+import chillhop from "./data";
+//Util
+import { playAudio } from "./util";
 
 function App() {
-  const [song , setSong] = React.useState(Data)  //Full Data Access 
-  const [currentsong , setCurrentsong] = React.useState(song[0])// We are setting the first song as the current song
-  const [isplaying , setIsplaying] = React.useState(false) // 
+  //Ref
+  const audioRef = useRef(null);
+
+  const [songs, setSongs] = useState(chillhop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+    animationPercentage: 0,
+    volume: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const percentage = Math.round((roundedCurrent / roundedDuration) * 100);
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration: duration,
+      animationPercentage: percentage,
+      volume: e.target.volume,
+    });
+  };
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    playAudio(isPlaying, audioRef);
+    return;
+  };
   return (
-    <div className="container">
-      <h1 id="sexy">Music Player By Nikola Tesla.</h1>
-      <Songs currentsong={currentsong}></Songs>
-      <Player currentsong={currentsong} isplaying={isplaying} setIsplaying={setIsplaying}></Player>
-      <Library songs={song} setCurrentsong={setCurrentsong}/>
-      </div>
+    <div className={`App ${libraryStatus ? "library-active" : ""}`}>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song isPlaying={isPlaying} currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        songInfo={songInfo}
+        setSongInfo={setSongInfo}
+        songs={songs}
+        setSongs={setSongs}
+        setCurrentSong={setCurrentSong}
+      />
+      <Library
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onLoadedMetadata={timeUpdateHandler}
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={songEndHandler}
+       
+      ></audio>
+    </div>
   );
 }
 
